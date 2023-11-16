0e6cd280c87d6d3979a1cb04a79ab13e31d0f27e
feforet <felix.foret@student.uclouvain.be>
2023-11-17 00:15:32 +0100

    lecteurs_ecrivains 1e version

--- p1/lecteurs_ecrivains.c
+++ p1/lecteurs_ecrivains.c
@@ -1,4 +1,3 @@
-#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
@@ -15,8 +14,8 @@ int readcount=0;
 int writecount=0;
 
 
-void writer(void) {
-    while(true) {
+void* writer(void* arg) {
+    for(int i = 0; i < 640; i++) {
         //prepare_data();
         
         pthread_mutex_lock(&mutex_write);
@@ -26,7 +25,7 @@ void writer(void) {
         
         sem_wait(&sem_write);
         // section critique
-        //write_database();
+        for (int i=0; i<10000; i++);
         sem_post(&sem_write);
         
         pthread_mutex_lock(&mutex_write);
@@ -34,25 +33,61 @@ void writer(void) {
         if (writecount==0) sem_post(&sem_read);
         pthread_mutex_unlock(&mutex_write);
    }
+   return NULL;
 }
 
 
-void reader(void) {
-    while(true) {
-        pthread_mutex_lock(&z); // un seul reader en attente sur rsem
+void* reader(void* arg) {
+    for(int i = 0; i < 2560; i++) {
+        pthread_mutex_lock(&z); // un seul reader en attente sur sem_read
         sem_wait(&sem_read);
         pthread_mutex_lock(&mutex_read);
         readcount++;
         if (readcount==1) sem_wait(&sem_write);
+        pthread_mutex_unlock(&mutex_read);
+        sem_post(&sem_read);
+        pthread_mutex_unlock(&z);
+
         // section critique
-        //read_database();
-        //
+        for (int i=0; i<10000; i++);
+
+        pthread_mutex_lock(&mutex_read);
+        readcount--;
+        if(readcount==0) sem_post(&sem_write);
+        pthread_mutex_unlock(&mutex_read);
+
+        //process_data();
     }
+    return NULL;
 }
 
 
 int main(int argc, char *argv[]) {
     sem_init(&sem_write, 0, 1);
     sem_init(&sem_read, 0, 1);
-    printf("blabla");
+
+    //int nWriters = atoi(argv[1]);
+    //int nReaders = atoi(argv[2]);
+
+    int nWriters = 10;
+    int nReaders = 10;
+
+    pthread_t* writers = malloc(nWriters * sizeof(pthread_t));
+    pthread_t* readers = malloc(nReaders * sizeof(pthread_t));
+    for (int i = 0; i < nWriters; i++) {
+        pthread_create(writers + i, NULL, &writer, NULL);
+    }
+    for (int i = 0; i < nReaders; i++) {
+        pthread_create(readers + i, NULL, &reader, NULL);
+    }
+
+    void* res;
+    for (int i = 0; i < nWriters; i++) {
+        pthread_join(writers[i], &res);
+    }
+    for (int i = 0; i < nReaders; i++) {
+        pthread_join(readers[i], &res);
+    }
+    
+    printf("fini !!\n");
 }
