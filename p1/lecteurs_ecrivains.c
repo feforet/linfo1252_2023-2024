8d60997b0b01c6d3804fea5f67cfe5bb0adb906e
feforet <felix.foret@student.uclouvain.be>
2023-11-17 18:27:59 +0100

    fin? read/write et prod/cons

--- p1/lecteurs_ecrivains.c
+++ p1/lecteurs_ecrivains.c
@@ -1,23 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-#include <errno.h>
-#include <stdbool.h>
 #include <semaphore.h>
 
-pthread_mutex_t mutex_read;
 pthread_mutex_t mutex_write;
+pthread_mutex_t mutex_read;
 pthread_mutex_t z;
 sem_t sem_write;
 sem_t sem_read;
-int readcount=0;
 int writecount=0;
+int readcount=0;
 
 
-void* writer(void* arg) {
+void* writer() {
     for(int i = 0; i < 640; i++) {
-        //prepare_data();
-        
         pthread_mutex_lock(&mutex_write);
         writecount++;
         if (writecount==1) sem_wait(&sem_read);
@@ -32,12 +28,11 @@ void* writer(void* arg) {
         writecount--;
         if (writecount==0) sem_post(&sem_read);
         pthread_mutex_unlock(&mutex_write);
-   }
-   return NULL;
+    }
 }
 
 
-void* reader(void* arg) {
+void* reader() {
     for(int i = 0; i < 2560; i++) {
         pthread_mutex_lock(&z); // un seul reader en attente sur sem_read
         sem_wait(&sem_read);
@@ -55,25 +50,24 @@ void* reader(void* arg) {
         readcount--;
         if(readcount==0) sem_post(&sem_write);
         pthread_mutex_unlock(&mutex_read);
-
-        //process_data();
     }
-    return NULL;
 }
 
 
 int main(int argc, char *argv[]) {
-    sem_init(&sem_write, 0, 1);
-    sem_init(&sem_read, 0, 1);
-
     //int nWriters = atoi(argv[1]);
     //int nReaders = atoi(argv[2]);
-
     int nWriters = 10;
     int nReaders = 10;
 
-    pthread_t* writers = malloc(nWriters * sizeof(pthread_t));
-    pthread_t* readers = malloc(nReaders * sizeof(pthread_t));
+    sem_init(&sem_write, 0, 1);
+    sem_init(&sem_read, 0, 1);
+    pthread_mutex_init(&z, NULL);
+    pthread_mutex_init(&mutex_write, NULL);
+    pthread_mutex_init(&mutex_read, NULL);
+
+    pthread_t writers[nWriters];
+    pthread_t readers[nReaders];
     for (int i = 0; i < nWriters; i++) {
         pthread_create(writers + i, NULL, &writer, NULL);
     }
@@ -90,4 +84,10 @@ int main(int argc, char *argv[]) {
     }
     
     printf("fini !!\n");
+
+    pthread_mutex_destroy(&mutex_write);
+    pthread_mutex_destroy(&mutex_read);
+    pthread_mutex_destroy(&z);
+    sem_destroy(&sem_write);
+    sem_destroy(&sem_read);
 }
--- p1/producteurs_consommateurs.c
+++ p1/producteurs_consommateurs.c
@@ -1,7 +1,83 @@
-#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
+#include <stdbool.h>
+#include <semaphore.h>
+
+#define BUF_SIZE 8
+#define N_DATA 8192
+
+
+pthread_mutex_t mutex;
+sem_t empty;
+sem_t full;
+int buffer[BUF_SIZE];
+int x = 0;
+int y = 0;
+
+void* producer () {
+    while(true) {
+        for (int i=0; i<10000; i++);
+        int item = rand();
+        sem_wait(&empty);
+        pthread_mutex_lock(&mutex);
+        if (x == N_DATA) {
+            pthread_mutex_unlock(&mutex);
+            sem_post(&full);
+            sem_post(&empty);
+            break;
+        }
+        //insert_item();
+        buffer[x++ % BUF_SIZE] = item;
+        pthread_mutex_unlock(&mutex);
+        sem_post(&full);
+    }
+}
+
+void* consumer () {
+    int res;
+    while (true) {
+        sem_wait(&full);
+        pthread_mutex_lock(&mutex);
+        if (y == N_DATA) {
+            pthread_mutex_unlock(&mutex);
+            sem_post(&empty);
+            sem_post(&full);
+            break;
+        }
+        //remove_item();
+        res = buffer[y++ % BUF_SIZE];
+        pthread_mutex_unlock(&mutex);
+        sem_post(&empty);
+        for (int i=0; i<10000; i++);
+    }
+}
 
 int main(int argc, char *argv[]) {
-    printf("blabla");
+    int nCons = atoi(argv[1]);
+    int nProd = atoi(argv[2]);
+    pthread_mutex_init(&mutex, NULL);
+    sem_init(&empty, 0, BUF_SIZE);
+    sem_init(&full, 0, 0);
+
+    pthread_t producers[nProd];
+    pthread_t consumers[nCons];
+    for (int i = 0; i < nProd; i++) {
+        pthread_create(producers + i, NULL, &producer, NULL);
+    }
+    for (int i = 0; i < nCons; i++) {
+        pthread_create(consumers + i, NULL, &consumer, NULL);
+    }
+
+    void* res;
+    for (int i = 0; i < nProd; i++) {
+        pthread_join(producers[i], &res);
+    }
+    for (int i = 0; i < nCons; i++) {
+        pthread_join(consumers[i], &res);
+    }
+
+    pthread_mutex_destroy(&mutex);
+    sem_destroy(&empty);
+    sem_destroy(&full);
 }
