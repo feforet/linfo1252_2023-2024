b084a3588ab23df52e99030de28ee3514dcc0ad6
feforet <felix.foret@student.uclouvain.be>
2023-11-24 09:17:45 +0100

    scripts bash

---
+++ p1/eval_perf_lect_ecr.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+TOMEASURE=lecteurs_ecrivains
+OUT=$TOMEASURE.csv
+NMes=5
+
+
+echo "nombre de coeurs,mesure" > $OUT
+for n in 2 4 8 16 32 64; do
+    for i in $(seq 1 $NMes); do
+        echo -n "$n," >> $OUT
+        /usr/bin/time -f "%e" -o $OUT -a ./$TOMEASURE $(($n/2)) $(($n/2))
+    done
+done
