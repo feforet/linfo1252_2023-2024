d34598f906d9155133fad8cf3b2f3d264358720f
feforet <felix.foret@student.uclouvain.be>
2023-12-01 13:22:21 +0100

    improve sem, new makefile for custom locks

--- p1/tache2/my_mutex.c
+++ p1/tache2/my_mutex.c
@@ -4,54 +4,40 @@
 #include "my_mutex.h"
 
 my_mutex_t mutx;
-
-static inline int my_test_and_set(int* verrou) {
-    __asm__(
-        "ENTER:\n\t"
-        "movl $1, %%eax\n\t"
-        "xchgl %%eax, %0\n\t"
-        "testl %%eax, %%eax\n\t"
-        "jnz ENTER\n\t"
-        :"+m" (*verrou)
-        :
-        :"%eax"
-    );
-}
+int nAccess;
 
 int my_mutex_init(my_mutex_t* mutex) {
     mutex->value = 0;
 }
 
 int my_mutex_lock_ts(my_mutex_t* mutex) {
+    int* value = &(mutex->value);
     __asm__(
         "ENTER:\n\t"
         "movl $1, %%eax\n\t"
         "xchgl %%eax, %0\n\t"
         "testl %%eax, %%eax\n\t"
         "jnz ENTER\n\t"
-        :"+m" (*mutex)
+        :"+m" (*value)
         :
         :"%eax"
     );
 }
 
 int my_mutex_lock_tts(my_mutex_t* mutex) {
+    int* value = &(mutex->value);
     __asm__(
         "ENTER:\n\t"
         "movl $1, %%eax\n\t"
         "xchgl %%eax, %0\n\t"
         "testl %%eax, %%eax\n\t"
-        "jz FIN \n\t"
-
-        "LOOP: \n\t"
-        "testl %0, %0 \n\t"
-        "jnz LOOP \n\t"
-        "jmp ENTER \n\t"
-
-
-        "FIN: \n\t"
-
-        :"+m" (*mutex)
+        "jz FIN\n\t"
+        "LOOP:\n\t"
+        "testl %0, %0\n\t"
+        "jnz LOOP\n\t"
+        "jmp ENTER\n\t"
+        "FIN:\n\t"
+        :"+m" (*value)
         :
         :"%eax"
 
@@ -72,23 +58,23 @@ int my_mutex_unlock(my_mutex_t* mutex) {
 
 int my_sem_init(my_sem_t* sem, int val) {
     sem->value = val;
-    my_mutex_init(sem->mutex);
+    my_mutex_init(sem->waiting);
+    my_mutex_init(sem->modifying);
 }
 
 int my_sem_wait(my_sem_t* sem) {
-    my_mutex_lock_ts(sem->mutex);
-    while (sem->value <= 0) {
-        my_mutex_unlock(sem->mutex);
-        my_mutex_lock_ts(sem->mutex);
-    }
+    my_mutex_lock_ts(sem->waiting);
+    while (sem->value <= 0) {}
+    my_mutex_lock_ts(sem->modifying);
     sem->value--;
-    my_mutex_unlock(sem->mutex);
+    my_mutex_unlock(sem->modifying);
+    my_mutex_unlock(sem->waiting);
 }
 
 int my_sem_post(my_sem_t *sem) {
-    my_mutex_lock_ts(sem->mutex);
+    my_mutex_lock_ts(sem->modifying);
     sem->value++;
-    my_mutex_unlock(sem->mutex);
+    my_mutex_unlock(sem->modifying);
 }
 
 void* func (void* arg) {
@@ -96,7 +82,6 @@ void* func (void* arg) {
     for (int i = 0; i < nAccess; i++) {
         my_mutex_lock_ts(&mutx);
         for (int i = 0; i < 10000; i++) {}
-        
         my_mutex_unlock(&mutx);
     }
     
@@ -104,7 +89,7 @@ void* func (void* arg) {
 
 int main(int argc, char *argv[]) {
     int nThreads = atoi(argv[1]);
-    int nAccess = 6400 / nThreads;
+    nAccess = 6400 / nThreads;
 
     my_mutex_init(&mutx);
 
